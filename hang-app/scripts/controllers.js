d63374cec3b623f64993442ff54fa0a7adfb7fa2
Joel <joeljossie@gmail.com>
2022-04-04 16:02:16 -0600

    refactored routes to use controllers

--- hang-app/scripts/controllers.js
+++ hang-app/scripts/controllers.js
@@ -1,12 +1,100 @@
+import {navigate} from "./routing.js";
+
 class Controller {
+    registerEventListeners(){}
+}
+
+
+export class StartController extends Controller {
+    constructor() {
+        super();
+    }
+
+    registerEventListeners() {
+        document.getElementById("start__newHangoutButton").addEventListener('touchend', (e) => {
+            // displayPage(view.renderLoginPage());
+            navigate("login");
+        });
+        document.getElementById("start__joinHangoutButton").addEventListener('touchend', (e) => {
+            // displayPage(view.renderJoinPage());
+            navigate("join");
+        });
+    }
+
+}
+
+export class JoinController extends Controller {
+    constructor() {
+        super();
+    }
+
+    registerEventListeners() {
+        document.getElementById("join__joinHangoutButton").addEventListener('touchend', (e) => {
+            // API Request with the Join Code
+            // displayPage(view.renderLoginPage());
+            navigate("login");
+        });
+    }
+
+}
+
+export class LoginController extends Controller {
+    constructor() {
+        super();
+    }   
+
+    registerEventListeners(){
+        document.getElementById("login__loginButton").addEventListener('touchend', (e) => {
+            // API Login Request
+            // navigate("pickDecision");
+        });
+    }
 
 }
 
 
-class joinController extends Controller {
+export class PickDecisionController extends Controller {
     constructor() {
-
+        super();
+    }   
+    
+    registerEventListeners(){
+        document.getElementById("decision__startButton").addEventListener('touchend', (e) => {
+            // API call?
+            navigate("suggest");
+        });
     }
 }
 
+export class SuggestController extends Controller {
+    constructor() {
+        super();
+    }
 
+    registerEventListeners() {
+        
+    }
+
+}
+
+export class VoteController extends Controller {
+    constructor() {
+        super();
+    }
+
+    registerEventListeners() {
+        
+    }
+
+}
+
+export class ResultController extends Controller {
+    constructor() {
+        super();
+    }
+
+    registerEventListeners() {
+        
+    }
+
+}
--- hang-app/scripts/routing.js
+++ hang-app/scripts/routing.js
@@ -6,69 +6,38 @@
 
 
 import * as view from "./views.js";
+import {JoinController, LoginController, PickDecisionController, ResultController, StartController, SuggestController, VoteController} from "./controllers.js";
 
 
 
-function displayPage(page, registerListenersCallback) {
+function displayPage(page, controller) {
     const root = document.getElementById("contentContainer");
     root.innerHTML = "";
     root.appendChild(page());
-    registerListenersCallback();
+    controller.registerEventListeners();
 }
 
 
 class Destination {
-    constructor(renderFunction, registerListenersFunction) {
+    constructor(renderFunction, controller) {
         this.renderFunction = renderFunction;
-        this.registerListenersFunction = registerListenersFunction;
+        this.controller = controller;
     }
 }
 
 
 const destinations = {
-    "start": new Destination(view.renderStartPage, () => {
-        document.getElementById("start__newHangoutButton").addEventListener('touchend', (e) => {
-            // displayPage(view.renderLoginPage());
-            navigate("login");
-        });
-        document.getElementById("start__joinHangoutButton").addEventListener('touchend', (e) => {
-            // displayPage(view.renderJoinPage());
-            navigate("join");
-        });
-    }),
-    "join": new Destination(view.renderJoinPage, () => {
-        document.getElementById("join__joinHangoutButton").addEventListener('touchend', (e) => {
-            // API Request with the Join Code
-            // displayPage(view.renderLoginPage());
-            navigate("login");
-        });
-    }),
-    "login": new Destination(view.renderLoginPage, () => {
-        document.getElementById("login__loginButton").addEventListener('touchend', (e) => {
-            // API Login Request
-            // navigate("pickDecision");
-        });
-    }),
-    "pickDecision": new Destination(view.renderPickDecisionPage, () => {
-        document.getElementById("decision__startButton").addEventListener('touchend', (e) => {
-            // API call?
-            navigate("suggest");
-        });
-
-    }),
-    "suggest": new Destination(view.renderSuggestPage, () => {
-
-    }),
-    "vote": new Destination(view.renderVotePage, () => {
-
-    }),
-    "result": new Destination(view.renderResultPage, () => {
-
-    })
+    "start": new Destination(view.renderStartPage, new StartController()),
+    "join": new Destination(view.renderJoinPage, new JoinController()),
+    "login": new Destination(view.renderLoginPage, new LoginController()),
+    "pickDecision": new Destination(view.renderPickDecisionPage, new PickDecisionController()),
+    "suggest": new Destination(view.renderSuggestPage, new SuggestController()),
+    "vote": new Destination(view.renderVotePage, new VoteController()),
+    "result": new Destination(view.renderResultPage, new ResultController())
 };
 
 
 export function navigate(destinationString) {
     const destination = destinations[destinationString];
-    displayPage(destination.renderFunction, destination.registerListenersFunction);
+    displayPage(destination.renderFunction, destination.controller);
 }
