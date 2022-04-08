0f7c7a7ffc5779f81ba2e9340af5e993bed5d7d8
Joel <joeljossie@gmail.com>
2022-04-07 19:32:19 -0600

    Styled voting a bit more

--- hang-app/scripts/components.js
+++ hang-app/scripts/components.js
@@ -3,9 +3,9 @@ export function renderOptionCard(optionText) {
     element.classList.add("optionDetailCard");
     element.innerHTML = /*html*/ `
         <form id="vote__voteForm">
-            <button type="submit" id="vote__YesButton" class="buttonVote">Yes</button>
+            <button type="submit" id="vote__YesButton" class="box buttonVote">Yes</button>
             <h2 id="vote__optionText" class="optionText">${optionText}</h2>
-            <button type="submit" id="vote__NoButton" class="buttonVote">No</button>
+            <button type="submit" id="vote__NoButton" class="box buttonVote">No</button>
         </form>
     `;
     return element;
@@ -13,7 +13,7 @@ export function renderOptionCard(optionText) {
 
 export function renderOptionListItem(option) {
     const element = document.createElement("div");
-    element.classList.add("optionListItem")
+    element.classList.add("optionListItem");
     element.innerHTML = /*html*/ `
         ${option['optionText']}
     `;
